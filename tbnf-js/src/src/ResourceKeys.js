c5db0e9da16bdbcbdeb8e8800702e9bc2ef4d5ea
thautwarm <twshere@outlook.com>
2022-08-06 15:53:23 +0800

    fix build new Fable.Sedlex and remove built js files

--- tbnf-js/src/src/ResourceKeys.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.ocaml_rts_file = void 0;
-const ocaml_rts_file = "ocaml.rts_file";
-exports.ocaml_rts_file = ocaml_rts_file;
