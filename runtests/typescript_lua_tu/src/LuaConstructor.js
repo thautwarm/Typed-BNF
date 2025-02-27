6225a3a24bce4bde7dbe086b549f940fd2ef9300
thautwarm <twshere@outlook.com>
2025-02-27 22:42:56 +0800

    use raw name for type tag

--- runtests/typescript_lua_tu/src/LuaConstructor.js
+++ runtests/typescript_lua_tu/src/LuaConstructor.js
@@ -64,147 +64,147 @@ exports.if_else = if_else;
 exports.params = params;
 __exportStar(require("./LuaRequire"), exports);
 function DotName(value0, value1) {
-    return { $type: 'DotName_t', value0, value1 };
+    return { $type: 'DotName', value0, value1 };
 }
 function MethodName(value0, value1) {
-    return { $type: 'MethodName_t', value0, value1 };
+    return { $type: 'MethodName', value0, value1 };
 }
 function VarName(value) {
-    return { $type: 'VarName_t', value };
+    return { $type: 'VarName', value };
 }
 function PositionalArgs(value0, value1) {
-    return { $type: 'PositionalArgs_t', value0, value1 };
+    return { $type: 'PositionalArgs', value0, value1 };
 }
 function StringArg(value) {
-    return { $type: 'StringArg_t', value };
+    return { $type: 'StringArg', value };
 }
 function TableArgs(value) {
-    return { $type: 'TableArgs_t', value };
+    return { $type: 'TableArgs', value };
 }
 function TableConstructor(value0, value1) {
-    return { $type: 'TableConstructor_t', value0, value1 };
+    return { $type: 'TableConstructor', value0, value1 };
 }
 function ElementField(value0) {
-    return { $type: 'ElementField_t', value0 };
+    return { $type: 'ElementField', value0 };
 }
 function IndexField(pos, value, item) {
-    return { $type: 'IndexField_t', pos, value, item };
+    return { $type: 'IndexField', pos, value, item };
 }
 function NameField(key, value) {
-    return { $type: 'NameField_t', key, value };
+    return { $type: 'NameField', key, value };
 }
 function Assignment(value0, value1, value2) {
-    return { $type: 'Assignment_t', value0, value1, value2 };
+    return { $type: 'Assignment', value0, value1, value2 };
 }
 function BreakStmt(value) {
-    return { $type: 'BreakStmt_t', value };
+    return { $type: 'BreakStmt', value };
 }
 function DoStmt(value0, value1) {
-    return { $type: 'DoStmt_t', value0, value1 };
+    return { $type: 'DoStmt', value0, value1 };
 }
 function EmptyStmt(value0) {
-    return { $type: 'EmptyStmt_t', value0 };
+    return { $type: 'EmptyStmt', value0 };
 }
 function ExprStmt(value) {
-    return { $type: 'ExprStmt_t', value };
+    return { $type: 'ExprStmt', value };
 }
 function ForInStmt(value0, value1, value2, value3) {
-    return { $type: 'ForInStmt_t', value0, value1, value2, value3 };
+    return { $type: 'ForInStmt', value0, value1, value2, value3 };
 }
 function ForRangeStmt(value0, value1, value2, value3) {
-    return { $type: 'ForRangeStmt_t', value0, value1, value2, value3 };
+    return { $type: 'ForRangeStmt', value0, value1, value2, value3 };
 }
 function GotoStmt(value0, value1) {
-    return { $type: 'GotoStmt_t', value0, value1 };
+    return { $type: 'GotoStmt', value0, value1 };
 }
 function IfStmt(value0, value1, value2, value3, value4) {
-    return { $type: 'IfStmt_t', value0, value1, value2, value3, value4 };
+    return { $type: 'IfStmt', value0, value1, value2, value3, value4 };
 }
 function LabelStmt(value) {
-    return { $type: 'LabelStmt_t', value };
+    return { $type: 'LabelStmt', value };
 }
 function RepeatStmt(value0, value1, value2) {
-    return { $type: 'RepeatStmt_t', value0, value1, value2 };
+    return { $type: 'RepeatStmt', value0, value1, value2 };
 }
 function ReturnStmt(value0, value1) {
-    return { $type: 'ReturnStmt_t', value0, value1 };
+    return { $type: 'ReturnStmt', value0, value1 };
 }
 function WhileStmt(value0, value1, value2) {
-    return { $type: 'WhileStmt_t', value0, value1, value2 };
+    return { $type: 'WhileStmt', value0, value1, value2 };
 }
 function Attr(value0, value1) {
-    return { $type: 'Attr_t', value0, value1 };
+    return { $type: 'Attr', value0, value1 };
 }
 function Bin(op, l, r) {
-    return { $type: 'Bin_t', op, l, r };
+    return { $type: 'Bin', op, l, r };
 }
 function Bool(value0, value1) {
-    return { $type: 'Bool_t', value0, value1 };
+    return { $type: 'Bool', value0, value1 };
 }
 function CallFunc(value0, value1) {
-    return { $type: 'CallFunc_t', value0, value1 };
+    return { $type: 'CallFunc', value0, value1 };
 }
 function CallMethod(value0, value1, value2) {
-    return { $type: 'CallMethod_t', value0, value1, value2 };
+    return { $type: 'CallMethod', value0, value1, value2 };
 }
 function Ellipse(value0) {
-    return { $type: 'Ellipse_t', value0 };
+    return { $type: 'Ellipse', value0 };
 }
 function Exponent(value0, value1) {
-    return { $type: 'Exponent_t', value0, value1 };
+    return { $type: 'Exponent', value0, value1 };
 }
 function FuncDef(pos, is_local, fname, params, body) {
-    return { $type: 'FuncDef_t', pos, is_local, fname, params, body };
+    return { $type: 'FuncDef', pos, is_local, fname, params, body };
 }
 function Index(value0, value1) {
-    return { $type: 'Index_t', value0, value1 };
+    return { $type: 'Index', value0, value1 };
 }
 function Inv(value0, value1) {
-    return { $type: 'Inv_t', value0, value1 };
+    return { $type: 'Inv', value0, value1 };
 }
 function Len(value0, value1) {
-    return { $type: 'Len_t', value0, value1 };
+    return { $type: 'Len', value0, value1 };
 }
 function Neg(value0, value1) {
-    return { $type: 'Neg_t', value0, value1 };
+    return { $type: 'Neg', value0, value1 };
 }
 function NestedExp(value0, value1) {
-    return { $type: 'NestedExp_t', value0, value1 };
+    return { $type: 'NestedExp', value0, value1 };
 }
 function Nil(value) {
-    return { $type: 'Nil_t', value };
+    return { $type: 'Nil', value };
 }
 function Not(value0, value1) {
-    return { $type: 'Not_t', value0, value1 };
+    return { $type: 'Not', value0, value1 };
 }
 function Num(value0) {
-    return { $type: 'Num_t', value0 };
+    return { $type: 'Num', value0 };
 }
 function String(value0) {
-    return { $type: 'String_t', value0 };
+    return { $type: 'String', value0 };
 }
 function TableExpr(value0) {
-    return { $type: 'TableExpr_t', value0 };
+    return { $type: 'TableExpr', value0 };
 }
 function UnsolvedBin(value) {
-    return { $type: 'UnsolvedBin_t', value };
+    return { $type: 'UnsolvedBin', value };
 }
 function Var(value) {
-    return { $type: 'Var_t', value };
+    return { $type: 'Var', value };
 }
 function block(suite, ret) {
-    return { $type: 'block_t', suite, ret };
+    return { suite, ret };
 }
 function range(low, high, step) {
-    return { $type: 'range_t', low, high, step };
+    return { low, high, step };
 }
 function if_elseif(pos, cond, elif_body) {
-    return { $type: 'if_elseif_t', pos, cond, elif_body };
+    return { pos, cond, elif_body };
 }
 function if_else(pos, else_body) {
-    return { $type: 'if_else_t', pos, else_body };
+    return { pos, else_body };
 }
 function params(names, vararg) {
-    return { $type: 'params_t', names, vararg };
+    return { names, vararg };
 }
 //# sourceMappingURL=LuaConstructor.js.map
